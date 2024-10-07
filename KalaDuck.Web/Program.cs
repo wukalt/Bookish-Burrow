4c623f71c6744d489e034758e21df2b7901412b6
Mehdi Mohseni <tty.mohseni@gmail.com>
2024-10-07 14:43:13 +0330

    Re-Structuring

--- KalaDuck.Web/Program.cs
+++ KalaDuck.Web/Program.cs
@@ -1,19 +1,7 @@
-using Microsoft.AspNetCore.Authentication.Negotiate;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-   .AddNegotiate();
-
-builder.Services.AddAuthorization(options =>
-{
-    // By default, all incoming requests will be authorized according to the default policy.
-    options.FallbackPolicy = options.DefaultPolicy;
-});
-builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
