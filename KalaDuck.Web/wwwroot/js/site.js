3c876713324688d3434992ecc8ab20fce7bf9a76
Soheil <soheilabbasnia@gmail.com>
2024-10-18 19:35:36 +0330

    Add: Humberger Menu for Header / Update: Responsive Anything in HomePage

--- KalaDuck.Web/wwwroot/js/site.js
+++ KalaDuck.Web/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Humberger Menu
+
+let menu = document.querySelector(".header_menu")
+let menuBtn = document.querySelector(".header-icon")
+let menuBtnIcon = document.querySelector(".header-icon i")
+
+menuBtn.addEventListener("click",function (){
+    if (menuBtnIcon.classList.contains("fa-bars")){
+        menu.style.left = "0"
+        menuBtnIcon.classList = "fa fa-times"
+    } else {
+        menu.style.left = "-256px"
+        menuBtnIcon.classList = "fa fa-bars"
+    }
+})
